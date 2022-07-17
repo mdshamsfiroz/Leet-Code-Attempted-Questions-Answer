1c54844efaae84456635dad2f28bbff30c5d9420
Md Shams Firoz <76830065+shamsfiroz@users.noreply.github.com>
2022-07-17 23:17:58 +0530

    Time: 2 ms (69.35%), Space: 41.3 MB (62.15%) - LeetHub

---
+++ 258-add-digits/258-add-digits.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public int addDigits(int num) {
+        while(num > 9){
+            int res =0;
+            while(num > 0){
+                res += num%10;
+                num /= 10;
+            }
+            num = res;
+        }
+
+        return num;
+
+    }
+}
