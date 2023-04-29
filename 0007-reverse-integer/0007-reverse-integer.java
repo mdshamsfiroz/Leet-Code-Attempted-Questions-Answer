84673b684e19f562e28b6a2e8ae6431621b2ab3e
Md Shams Firoz <76830065+mdshamsfiroz@users.noreply.github.com>
2023-04-29 12:01:30 +0530

    Time: 1 ms (100.00%), Space: 40.2 MB (28.00%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int reverse(int n) {
+        int res=0;
+        
+        while(n!=0){
+            int digit =n%10;
+           int newres =res*10+digit;
+            if( newres /10 != res){
+                // integer overflow
+                return 0;
+            }
+            res=newres;
+            n/=10;
+        }
+        return res;
+        }
+    }
