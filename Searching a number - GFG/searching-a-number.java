e6814aa89bf902ecba0dd6891a46b2c809490889
Md Shams Firoz <76830065+mdshamsfiroz@users.noreply.github.com>
2023-05-10 13:30:50 +0530

    Added solution - LeetHub

---
+++ Searching a number - GFG/searching-a-number.java
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class Main {
+    // Driver code
+    public static void main(String[] args) throws Exception {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while (t-- > 0) {
+            String[] str = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(str[0]);
+            int k = Integer.parseInt(str[1]);
+            str = br.readLine().trim().split(" ");
+            int arr[] = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(str[i]);
+            }
+
+            int ans = new Solution().search(arr, n, k);
+
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public int search(int arr[], int n, int k) {
+        // code here
+        for(int idx=0;idx< arr.length;idx++){
+            if(arr[idx]==k){
+                return idx+1;
+            }
+        }
+        return -1;
+    }
+}
