c8be9810c02062a73200f5838f0e056ba99c894d
Md Shams Firoz <76830065+mdshamsfiroz@users.noreply.github.com>
2023-05-17 17:53:42 +0530

    Time: 0 ms (100.00%), Space: 43.1 MB (8.13%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.java
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.java
@@ -1,11 +1,10 @@
 class Solution {
     public int[] runningSum(int[] nums) {
-        //empty array on spot ask can i handle corner case like empty case)   
-         for(int idx =1; idx<nums.length; idx++){
-             nums[idx]+=nums[idx-1];
-         }
-        return nums;
-        
-        // we must have to follow the design principal so (this is wrong step)
+     int [] prefix = new int [nums.length];
+    
+    for(int idx=0; idx<nums.length; idx++){
+        prefix[idx]= nums[idx] + ((idx>0) ? prefix[idx-1]:0);
+    }
+      return prefix;  
     }
 }
