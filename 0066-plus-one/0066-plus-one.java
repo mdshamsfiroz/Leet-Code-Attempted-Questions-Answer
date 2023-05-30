314852c77d424a6554d772610e34309282e3dd3b
Md Shams Firoz <76830065+mdshamsfiroz@users.noreply.github.com>
2023-05-30 23:12:49 +0530

    Time: 0 ms (100.00%), Space: 41.4 MB (11.56%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,6 +1,6 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-       for( int idx= digits.length -1; idx>=0;idx--){
+       for( int idx= digits.length -1; idx>=0;idx--){ //>= because 0 will ber also taken care
            if(digits[idx] == 9){
                digits[idx] = 0;
            }
