6a05857afbbed124cea9e24f7ee4f6ba143511fc
Md Shams Firoz <76830065+mdshamsfiroz@users.noreply.github.com>
2023-05-31 23:24:31 +0530

    Time: 5 ms (95.82%), Space: 41.8 MB (91.80%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean mapOneToOne(String s1, String s2) {
+        char [] map=new char[256];
+        //ascii code
+        
+        for(int idx=0; idx<s1.length();idx++){
+            char ch1=s1.charAt(idx);
+            char ch2=s2.charAt(idx);
+            
+            if(map[ch1] != '\u0000' && map[ch1] !=ch2){
+                return false;
+            }
+                map[ch1] = ch2;
+            
+            
+          
+        }
+          return true;
+    }
+        
+    public boolean isIsomorphic(String s1, String s2){
+        return mapOneToOne(s1, s2) && mapOneToOne(s2,s1);
+    }
+    
+}
