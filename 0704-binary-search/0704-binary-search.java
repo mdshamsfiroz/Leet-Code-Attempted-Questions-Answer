f9fba376c8fbf0391891a5f53efbd96b9e503a9f
Md Shams Firoz <76830065+mdshamsfiroz@users.noreply.github.com>
2023-06-27 22:39:31 +0530

    Time: 0 ms (100.00%), Space: 44.8 MB (50.80%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int left=0,right=nums.length -1;
+        
+        while(left<=right){
+            int mid=left+(right-left)/2;
+             if(nums[mid]==target){
+                 return mid;
+             }
+            else if(nums[mid] < target){
+                left=mid+1;
+                //disccard left to mid region
+            }
+            else{
+                right=mid-1;
+                //discard mid to right region
+            }
+        }
+        return -1;
+        }
+    
+}
