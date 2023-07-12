1dd78166bd96d3a92722666651b61fb74947e66b
Md Shams Firoz <76830065+mdshamsfiroz@users.noreply.github.com>
2023-07-12 22:08:45 +0530

    Added solution - LeetHub

---
+++ Power Of Numbers - GFG/power-of-numbers.java
@@ -0,0 +1,73 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+class Main {
+    
+    // compute reverse of a number
+    public static long rev(long n)
+    {
+        long rev_num = 0;
+         while(n > 0) 
+          { 
+            rev_num = rev_num*10 + n%10; 
+            n = n/10; 
+          } 
+          return rev_num;
+    }
+    
+	public static void main (String[] args) {
+		Scanner sc=new Scanner(System.in);
+		
+        //testcases
+		int T=sc.nextInt();
+		while(T-->0)
+		{
+		    Solution obj=new Solution();
+		    
+		    int N;
+		    
+		    
+            //input N
+		    N=sc.nextInt();
+		    int R=0;
+		    
+		    	    
+            // reverse the given number n
+		    R=(int)rev(N);
+	
+            //power of the number to it's reverse
+		    long ans=(long)obj.power(N,R);
+		    System.out.println(ans);
+		    
+		    
+		}
+		
+	}
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    static int mod = 1000000007;
+        
+   static  long power(int N,int R)
+    {
+        //Your code here
+        
+        if(R==1) return N%mod;
+        long half = power(N,R/2);
+        half = (half*half) %mod;
+        if(R%2 ==1) half = (half*N)%mod;
+        return half;
+    }
+
+}
