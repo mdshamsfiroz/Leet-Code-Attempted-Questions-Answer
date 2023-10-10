7990a976b843b16333e9cabd064a89205ce34ec5
Md Shams Firoz <76830065+mdshamsfiroz@users.noreply.github.com>
2023-10-10 14:23:09 +0530

    Time: 0 ms (100.00%), Space: 44.1 MB (91.46%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,19 +1,49 @@
 class Solution {
-    //it can also solved from bionary search.
-    public int[] searchRange(int[] arr, int target) {
-         int firstIndex=-1 , lastIndex=-1;
-        
-        for(int idx=0;idx<arr.length;idx++){
-            if(arr[idx]==target){
-                lastIndex=idx;
-                
-                if(firstIndex == -1){
-                    firstIndex = idx;
+    public int[] searchRange(int[] nums, int target) {
+        int first = findFirst(nums, target);
+        int last = findLast(nums, target);
+        return new int[]{first, last};
+    }
+
+    private int findFirst(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        int first = -1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                if (mid == 0 || nums[mid - 1] != target) {
+                    first = mid;
+                    break;
+                } else {
+                    right = mid - 1;
                 }
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
             }
         }
-        
-        int [] ans ={firstIndex,lastIndex};
-        return ans;
+        return first;
+    }
+
+    private int findLast(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        int last = -1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                if (mid == nums.length - 1 || nums[mid + 1] != target) {
+                    last = mid;
+                    break;
+                } else {
+                    left = mid + 1;
+                }
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return last;
     }
 }
