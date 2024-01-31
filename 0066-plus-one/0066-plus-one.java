4c4b1bc52c73fb8b797065d96f0d13258d7ccac3
Md Shams Firoz <76830065+mdshamsfiroz@users.noreply.github.com>
2024-01-31 22:31:20 +0530

    Time: 0 ms (100.00%), Space: 41.9 MB (28.76%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,19 +1,15 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-       for( int idx= digits.length -1; idx>=0;idx--){ //>= because 0 will ber also taken care
-           if(digits[idx] == 9){
-               digits[idx] = 0;
-           }
-           else{
-               digits[idx]++;
-               return digits;
-               
-           }
-       } 
-        
-        // corner case : Imput has all digits equal to 9
-        int [] res= new int[digits.length +1];
-        res[0] =1;
-        return res;
+    for (int i = digits.length - 1; i >= 0; i--) {
+	if (digits[i] < 9) {
+		digits[i]++;
+		return digits;
+	}
+	digits[i] = 0;
+    }
+
+digits = new int[digits.length + 1];
+digits[0] = 1;
+return digits;
     }
 }
